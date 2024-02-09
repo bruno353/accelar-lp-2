c7dacd88da9a44988f3fbd62ac3e3c3bc1f8bca7
Bruno Laureano <brunolsantos152@gmail.com>
2024-02-09 14:37:17 -0300

    fix: removing bg color

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -66,9 +66,9 @@ const Contact = () => {
                     </div>
                   </div> */}
                   <div className="w-full px-4">
-                    <button className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+                    <a  href="mailto:contact@accelar.io" className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
                       Submit Ticket
-                    </button>
+                    </a>
                   </div>
                 </div>
               </form>

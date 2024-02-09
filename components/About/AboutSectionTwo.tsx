4f2a1cd1178e23f8d5e7c306f514def88ed2e284
Bruno Laureano <brunolsantos152@gmail.com>
2024-02-09 16:52:02 -0300

    fix: removing bg color

--- components/About/AboutSectionTwo.tsx
+++ components/About/AboutSectionTwo.tsx
@@ -113,7 +113,7 @@ const AboutSectionTwo = () => {
             </div>
             <div className="w-full px-4 lg:w-1/2">
               <div
-                className="wow fadeInUp relative mx-auto mb-12 aspect-[34/24] max-w-[500px] text-center lg:m-0"
+                className="wow fadeInUp  relative mx-auto mb-12 aspect-[34/24] max-w-[500px] text-center lg:m-0"
                 data-wow-delay=".15s"
               >
                 <Image

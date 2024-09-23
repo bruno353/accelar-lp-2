bd038a8ed5aa505d74b98c2142bd30dc5fb67e99
Bruno Santos <brunolsantos152@gmail.com>
2024-09-23 10:33:46 -0300

    feat: accelar new page

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -10,7 +10,7 @@ const Features = () => {
         id="features"
         className="bg-transparent relative z-20 py-16 md:py-20 lg:py-28 lg:pb-14 3xl:py-28"
       >
-        <ContainerScroll titleComponent={'Yes'} />
+        <ContainerScroll titleComponent={<h2>Optimize your product cycle.</h2>} children={<p>From voice-chat features to fullstack hosting. A collaborative environment where your team can exchange ideas and make decisions quickly.</p>} />
         <div className="container max-w-[1200px] 3xl:max-w-[1400px]">
           <SectionTitle
             title="Optimize your product cycle."

03553bb10cf850691ea9902b8ee61156c8c00edd
Bruno Santos <brunolsantos152@gmail.com>
2026-01-01 13:33:05 -0300

    feat: doing changes

--- app/head.tsx
+++ app/head.tsx
@@ -4,8 +4,16 @@ export default function Head() {
       <title>Accelar</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Accelar" />
-      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      <link rel="alternate icon" href="/favicon.ico" />
+      <link rel="icon" href="/images/new-logo.svg" />
+
+      {/* Open Graph / Google Search - usar imagem quadrada com fundo transparente */}
+      <meta property="og:image" content="/images/og-logo.svg" />
+      <meta property="og:image:width" content="512" />
+      <meta property="og:image:height" content="512" />
+      <meta property="og:image:type" content="image/svg+xml" />
+
+      {/* Apple Touch Icon */}
+      <link rel="apple-touch-icon" href="/images/og-logo.svg" />
     </>
   );
 }
